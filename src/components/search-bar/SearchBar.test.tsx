7fa61edf78062baa84364e025af4b23e6a3b6c49
NursultanFront <nursultan020395@gmail.com>
2023-03-06 19:21:34 +0500

    feat: add test to SearchBar

---
+++ src/components/search-bar/SearchBar.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import SearchBar from './SearchBar';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<SearchBar />);
+
+    // check if App components renders headline
+  });
+});
