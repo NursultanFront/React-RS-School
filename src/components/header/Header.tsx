ee9e722c15836b2b56825856c37f443e1402db72
NursultanFront <nursultan020395@gmail.com>
2023-03-19 15:28:00 +0500

    fix: fix header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,14 +12,20 @@ class Header extends Component<WithRouterProps> {
 
     return pathname.slice(1);
   };
+
   render() {
     return (
-      <header>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-        <p>
-          And now you are in <b>{this.getLocation()}</b> here right now
-        </p>
+      <header className="header">
+        <div className="header__wrapper">
+          <div className="header__links">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="about">About</NavLink>
+          </div>
+
+          <p>
+            <b>{this.getLocation()}</b>
+          </p>
+        </div>
       </header>
     );
   }
