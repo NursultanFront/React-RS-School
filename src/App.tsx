0e6f2c9a074ce4c0b0ced07e06cd956808b9e4ea
NursultanFront <nursultan020395@gmail.com>
2023-03-19 16:14:02 +0500

    fix: es lint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import SearchBar from './components/search-bar/SearchBar';
 import './App.css';
-import Cards from './components/cards/Cards';
-import { products } from './data/cards';
 import Header from './components/header/Header';
 import { Outlet } from 'react-router-dom';
 
--- src/components/search-bar/SearchBar.test.tsx
+++ src/components/search-bar/SearchBar.test.tsx
@@ -1,5 +1,5 @@
-import { fireEvent, getByDisplayValue, render, screen } from '@testing-library/react';
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import SearchBar from './SearchBar';
 
 describe('Testing SearchBar', () => {
