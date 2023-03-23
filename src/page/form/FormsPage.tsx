6122ddf49f4570473e988b97dd35f5f7ae58ef89
NursultanFront <nursultan020395@gmail.com>
2023-03-23 22:15:22 +0500

    fix: states

--- src/page/form/FormsPage.tsx
+++ src/page/form/FormsPage.tsx
@@ -3,18 +3,33 @@ import React, { Component } from 'react';
 import Form from '../../components/form/Form';
 import { InputField } from '../../components/formInputs/InputField';
 
-interface State {
+interface Props {
+  message?: string;
+}
+
+interface Input {
   inputTextError: boolean;
   inputDateError: boolean;
   inputFileError: boolean;
 }
 
-export default class FormsPage extends Component<Record<string, unknown>, State> {
-  state: State = {
-    inputTextError: false,
-    inputDateError: false,
-    inputFileError: false,
-  };
+interface State {
+  formState: boolean;
+  inputState: Input;
+}
+
+export default class FormsPage extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      formState: false,
+      inputState: {
+        inputTextError: false,
+        inputDateError: false,
+        inputFileError: false,
+      },
+    };
+  }
 
   form = React.createRef<HTMLFormElement>();
   inputText = React.createRef<HTMLInputElement>();
@@ -22,33 +37,57 @@ export default class FormsPage extends Component<Record<string, unknown>, State>
   inputFile = React.createRef<HTMLInputElement>();
 
   checkState = () => {
-    return Object.values(this.state).every((item) => item === false);
+    const state = Object.values(this.state.inputState).every((item) => item === false);
+    console.log(Object.values(this.state.inputState).every((item) => item === false));
+    console.log(this.state.inputState);
+    console.log(state);
+    if (state) {
+      this.setState((prevState: State) => {
+        return {
+          ...prevState,
+          formState: true,
+        };
+      });
+    }
   };
 
-  getForm = (event: React.ChangeEvent<HTMLFormElement>) => {
+  getForm = async (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
-    this.removeValidation();
-    this.validation();
-    if (this.checkState()) {
+    await this.removeValidation();
+    await this.validation();
+    await this.checkState();
+
+    if (this.state.formState) {
       this.form.current?.reset();
     }
   };
 
   removeValidation = () => {
-    this.setState({ inputDateError: false, inputTextError: false, inputFileError: false });
+    this.setState((prevState: State) => {
+      return {
+        ...prevState,
+        inputState: { inputTextError: false, inputDateError: false, inputFileError: false },
+      };
+    });
   };
 
   validation = () => {
     if (this.inputText.current?.value == '') {
-      this.setState({ inputTextError: true });
+      this.setState((prevState: State) => {
+        return { ...prevState, inputState: { ...prevState.inputState, inputTextError: true } };
+      });
     }
 
     if (this.inputDate.current?.value == '') {
-      this.setState({ inputDateError: true });
+      this.setState((prevState: State) => {
+        return { ...prevState, inputState: { ...prevState.inputState, inputDateError: true } };
+      });
     }
 
     if (this.inputFile.current?.files?.length == 0) {
-      this.setState({ inputFileError: true });
+      this.setState((prevState: State) => {
+        return { ...prevState, inputState: { ...prevState.inputState, inputFileError: true } };
+      });
     }
   };
   render() {
@@ -57,20 +96,20 @@ export default class FormsPage extends Component<Record<string, unknown>, State>
         <div>
           <form ref={this.form} onSubmit={this.getForm}>
             <InputField
-              showError={this.state.inputTextError}
+              showError={this.state.inputState.inputTextError}
               errorMessage="Validate"
               type="text"
               refProp={this.inputText}
             ></InputField>
             <InputField
-              showError={this.state.inputDateError}
+              showError={this.state.inputState.inputDateError}
               errorMessage="Validate"
               type="date"
               refProp={this.inputDate}
             ></InputField>
             <InputFile
               type="file"
-              showError={this.state.inputFileError}
+              showError={this.state.inputState.inputFileError}
               errorMessage="Validate"
               refProp={this.inputFile}
             ></InputFile>
@@ -78,6 +117,8 @@ export default class FormsPage extends Component<Record<string, unknown>, State>
             <div>
               <button type="submit"></button>
             </div>
+
+            <p>{this.state.formState && 'Проверка прошла'}</p>
           </form>
         </div>
       </>
