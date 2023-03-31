3451dcf30b683ae6a40ebdd6befeaa190e4d0fd6
NursultanFront <nursultan020395@gmail.com>
2023-03-31 16:58:18 +0500

    refactor: Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { withRouter } from '../../helper/withRouter';
-import { WithRouterProps } from '../../helper/withRouter';
+import { useLocation } from 'react-router-dom';
 
-class Header extends Component<WithRouterProps> {
-  getLocation = () => {
-    const pathname = this.props.location.pathname;
+const Header = () => {
+  const location = useLocation();
+  const getLocation = () => {
+    const pathname = location.pathname;
     if (pathname == '/') {
       return 'home';
     }
@@ -13,23 +12,21 @@ class Header extends Component<WithRouterProps> {
     return pathname.slice(1);
   };
 
-  render() {
-    return (
-      <header className="header">
-        <div className="header__wrapper">
-          <div className="header__links">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="about">About</NavLink>
-            <NavLink to="form">Form</NavLink>
-          </div>
-
-          <p>
-            <b>{this.getLocation()}</b>
-          </p>
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="header__links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="form">Form</NavLink>
         </div>
-      </header>
-    );
-  }
-}
 
-export default withRouter(Header);
+        <p>
+          <b>{getLocation()}</b>
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
